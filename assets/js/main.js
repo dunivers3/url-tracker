6507f0cf8491460abe9d066ea990856edae30025
Duncan <wangari.dun@gmail.com>
2022-08-31 15:24:04 +0300

    updates extension to completion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,60 @@
-const buttonEl = document.getElementById("inputBtn")
-const inputEl = document.getElementById("inputEl")
 let tracked = []
-let ulEl = document.querySelector(".ulEl")
+const inputEl = document.getElementById("inputEl")
+const buttonEl = document.getElementById("inputBtn")
+const ulEl = document.querySelector(".ulEl")
+const eventsInStorage = JSON.parse(localStorage.getItem("tracked"))
+const currentUrl = document.getElementById("saveBtn")
+const deleteBtn = document.getElementById("deleteBtn")
 
-buttonEl.addEventListener("click", () => {
-    tracked.push(inputEl.value)
-    renderTracked()
+if (eventsInStorage) {
+    tracked = eventsInStorage
+    renderTracked(tracked)
+}
 
-
+currentUrl.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        tracked.push(tabs[0].url)
+        localStorage.setItem("tracked", JSON.stringify(tracked))
+        renderTracked(tracked)
+    })
 })
 
-function renderTracked() {
+//render the urls in the dom
+function renderTracked(data) {
     let items = ""
-    tracked.forEach(element => {
-        items += `<li>${element}</li>`
+    data.forEach(element => {
+        items +=
+            `<li>
+                <a target="_blank" href="${element}">
+                ${element}
+                </a>
+            </li>`
     })
     ulEl.innerHTML = items
+}
+
+//listen for the save leads button click
+buttonEl.addEventListener("click", () => {
+    buttonAction()
+})
+
+//listen for the enter key press (on the save leads button)
+inputEl.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        buttonAction()
+    }
+})
+
+//listen for delete button click(double click)
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear()
+    tracked = []
+    renderTracked(tracked)
+})
+
+function buttonAction() {
+    tracked.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("tracked", JSON.stringify(tracked))
+    renderTracked(tracked)
 }
